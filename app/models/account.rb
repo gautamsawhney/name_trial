51b527f43409dcd246242956dac8ea19f1b7ff6c
gautamsawhney <gsawhney21@gmail.com>
2016-06-23 11:38:43 +0530

    Added Account model

---
+++ app/models/account.rb
@@ -0,0 +1,15 @@
+class Account < ApplicationRecord
+
+  # Autocode: Callbacks
+    
+  # Autocode: Relationships
+  
+  # Autocode: Accept Nested Attributes
+
+  # File Upload
+
+  # Autocode: Validations
+
+	# Soft Destroy
+
+end
